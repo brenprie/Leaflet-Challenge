cc4277e498d48dc9069a48062ddc815e7714bfe9
BrenPrie <brenprie@gmail.com>
2025-02-10 18:15:47 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-// Basemap tile layer options, to serve as background map
+// Basemap tile layers, to serve as background map options
 var Esri_NatGeoWorldMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 	maxZoom: 16
